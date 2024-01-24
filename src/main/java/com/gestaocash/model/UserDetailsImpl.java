3fd50ca4892cc1171fd0f05df0621ae117cf7deb
Leandro Costa <lcosta200212@gmail.com>
2024-01-24 18:24:11 -0300

    feat: alteracoes na config do security

--- src/main/java/com/gestaocash/model/UserDetailsImpl.java
+++ src/main/java/com/gestaocash/model/UserDetailsImpl.java
@@ -1,37 +1,47 @@
 package com.gestaoCash.model;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.gestaoCash.repositories.UserRepository;
 
-
 public class UserDetailsImpl implements UserDetails {
 
     private static final long serialVersionUID = 1L;
-	
-	private Users users;
+
+    private Users users;
+
     public UserDetailsImpl(Users user) {
         this.users = user;
     }
-	
-//	public UserDetailsImpl(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
+
+    // public UserDetailsImpl(UserRepository userRepository) {
+    // this.userRepository = userRepository;
+    // }
+
+    // @Override
+    // public Collection<? extends GrantedAuthority> getAuthorities() {
+    // String perfil = users.getTipoUsuario().equals("admin") ? "USER_ADMIN" :
+    // "USER_COMUM";
+
+    // return AuthorityUtils.createAuthorityList(perfil);
+
+    // }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-    	String perfil = users.getTipoUsuario().equals("Adm") ?
-                "ADMIN" :
-                "USER";
-     
-            return AuthorityUtils.createAuthorityList(perfil);
-    	
+
+        if (users.getTipoUsuario().equals("admin"))
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_COMUM"));
+        else
+            return List.of(new SimpleGrantedAuthority("ROLE_COMUM"));
     }
 
     @Override
@@ -43,8 +53,7 @@ public class UserDetailsImpl implements UserDetails {
     public String getUsername() {
         return users.getEmail();
     }
-    
-    
+
     public Users getDetailsLog() {
         return users;
     }
@@ -71,5 +80,5 @@ public class UserDetailsImpl implements UserDetails {
         // TODO Auto-generated method stub
         return true;
     }
-    
+
 }
